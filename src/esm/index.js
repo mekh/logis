61c9a07597c0016a926331f0656f85e12122437e
Alexandr <alexandr.mekh@gmail.com>
2020-11-11 10:46:52 +0200

    esm/cjs compatibility added

---
+++ src/esm/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable prefer-destructuring */
+import loggis from '../index';
+
+export const getLogger = loggis.getLogger;
+export const configure = loggis.configure;
+
+export default loggis;
