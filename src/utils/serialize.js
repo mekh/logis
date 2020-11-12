c260f3e324fbbc7aad5120bedbdaac82a7b31ff1
Alexandr <alexandr.mekh@gmail.com>
2020-11-12 11:44:28 +0200

    log Error instances

--- src/utils/serialize.js
+++ src/utils/serialize.js
@@ -2,6 +2,10 @@
  * Serialize a log item
  */
 module.exports = (data) => {
+  if (data instanceof Error) {
+    return data;
+  }
+
   if (data !== null && ['object', 'symbol'].includes(typeof data)) {
     try {
       return JSON.stringify(data);
