c934898494566b05f6aa2efde816d4cc2f1b0e04
Alexandr Mekh <alexandr.mekh@gmail.com>
2022-09-03 09:47:48 +0300

    remove jest-console, remove windows from tests gitlab action

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-const { CustomConsole } = require('@jest/console');
-
-const formatter = (type, message) => `console.${type} => ${message}`;
-
-global.console = new CustomConsole(process.stdout, process.stderr, formatter);
